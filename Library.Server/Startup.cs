221b75db3f8cc86a060e81802455a70c8011a1a3
Kolawole Campbell <kcampbell@becht.com>
2020-07-10 08:30:32 -0400

    Revert "Setting up single host."
    This reverts commit 53eaa0265ad378fba286875c1056ba5956a7da7b.

--- Library.Server/Startup.cs
+++ Library.Server/Startup.cs
@@ -63,7 +63,6 @@ namespace Library.Server
                 app.UseDeveloperExceptionPage();
                 app.UsePlayground(new PlaygroundOptions { Path = "/playground", QueryPath = "/graphql" });
             }
-            app.UseBlazorFrameworkFiles();
 
             app.UseHttpsRedirection();
 
