381bd4be72f318eb277cf7316dc0293f7a1b9f98
Kolawole Campbell <kolawole.campbell@gmail.com>
2020-07-11 23:39:27 -0400

    Joined books and documents for better queries.

---
+++ Library.Data/Helpers/DocUnionBook.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Library.Data.Helpers
+{
+    public class DocUnionBook
+    {
+        public int Docid { get; set; }
+        public string Isbn { get; set; }
+        public string PName { get; set; }
+        public DateTime PDate { get; set; }
+    }
