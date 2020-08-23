e247d116d398deb587bf53e7715852b7953c65bc
Kolawole Campbell <kolawole.campbell@gmail.com>
2020-08-23 17:05:29 -0400

    added google books

--- Library.Client/Program.cs
+++ Library.Client/Program.cs
@@ -8,20 +8,32 @@ using Library.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace Library.Client
 {
     public class Program
     {
+        private const string BackendUrl = "https://localhost:6060";
+        private const string GoogleApiKey = "AIzaSyBtAnGsK3npIDrpwpiD27el55QDdAYtjXc";
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri("https://localhost:6060/") });
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(BackendUrl) });
             builder.Services.AddHttpClient("LibraryClient", (services, client) =>
             {
-                client.BaseAddress = new Uri("https://localhost:6060/graphql");
+                client.BaseAddress = new Uri($"{BackendUrl}/graphql");
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", services.GetRequiredService<ITokenStore>().GetToken());
+            });
+            builder.Services.AddHttpClient("GoogleBooks", (services, client) =>
+            {
+                var queryParams = new Dictionary<string, string>() { { "key", GoogleApiKey }, };
+                var url = QueryHelpers.AddQueryString($"https://www.googleapis.com/books/v1/volumes", queryParams);
+                services.GetRequiredService<ILogger>().LogInformation($"Google Books: :{url}");
+                client.BaseAddress = new Uri(url);
             });
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddLibraryClient();
